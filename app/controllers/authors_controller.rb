4e72df35c1ab8f310a74652d45f2954f3721c7a1
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-11-30 01:41:09 +0300

    lab complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
   def show
     author = Author.find(params[:id])
 
@@ -7,9 +8,11 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
+    
     render json: author, status: :created
+   
   end
 
   private
@@ -17,5 +20,10 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+  def render_invalid_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+  
   
 end
